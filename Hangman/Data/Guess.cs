88a7bdd933e090ae8e011473c4d8a0e7a921899d
Aaron Gordon <adgordon@live.com>
2018-03-05 22:26:41 -0800

    Hangman framework in place

---
+++ Hangman/Data/Guess.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hangman.Data
+{
+    public class Guess
+    {
+        private char character;
+
+        public int Count
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public char Character
+        {
+            get
+            {
+                return character;
+            }
+            set
+            {
+                character = char.ToLower(value);
+            }
+        }
+    }
+}
--- Hangman/Pages/Index.cshtml.cs
+++ Hangman/Pages/Index.cshtml.cs
@@ -1,15 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Hangman.Data;
 
 namespace Hangman.Pages
 {
-    public class Index : PageModel
+    public class IndexModel : PageModel
     {
-        public string Message { get; private set; } = "PageModel.Message";
+        [BindProperty]
+        public Guess Guess { get; set; }
+
+        public string Word { get; private set; }
+        public int GuessCount { get; private set; }
 
         public void OnGet()
         {
-            Message += $" Server time is {DateTime.Now}";
+            Word = "_ _ _ _ _ _ _ _";
+            GuessCount = 0;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Guess.Count >= 7)
+                {
+                    Word = "Out of Guesses!";
+                    return Page();
+                }
+
+                GuessCount = Guess.Count + 1;
+            }
+
+            return Page();
         }
     }
 }
