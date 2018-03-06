371aeadff8b91448096b655b2b9c029a882d6a5f
Aaron Gordon <adgordon@live.com>
2018-03-05 20:09:00 -0800

    Added ASP.NET Core framework

---
+++ Hangman/Pages/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace Hangman.Pages
+{
+    public class Index : PageModel
+    {
+        public string Message { get; private set; } = "PageModel.Message";
+
+        public void OnGet()
+        {
+            Message += $" Server time is {DateTime.Now}";
+        }
+    }
+}
